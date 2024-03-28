47afee8b723ae2e5cfc7cfb3a7b6f6e5c9aceaa3
Polina Chetnikova <38739507+pchova@users.noreply.github.com>
2024-03-27 21:33:52 -0700

    created getComputerChoice() function

---
+++ rps.js
@@ -0,0 +1,8 @@
+function getComputerChoice() {
+    let options = ['rock', 'paper', 'scissors'];
+    let choice = Math.floor(Math.random() * options.length);
+
+    return options[choice];
+}
+
+console.log(getComputerChoice());
