395ab44f032b18d6e0558c0ef2d979a53cdc0c3f
Polina Chetnikova <38739507+pchova@users.noreply.github.com>
2024-03-27 21:48:24 -0700

    added playRound() function which plays a single round of RPS between user and computer and displays who wins

--- rps.js
+++ rps.js
@@ -5,4 +5,50 @@ function getComputerChoice() {
     return options[choice];
 }
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
+
+    
+function playRound(userChoice, computerChoice) {
+    let user = userChoice.toLowerCase();
+    let computer = computerChoice.toLowerCase();
+
+    console.log("User\'s Choice: " + user);
+    console.log("Computer\'s Choice: " + computer + "\n");
+    if (user == 'rock') {
+        if (computer == 'paper') {
+        console.log('Computer wins.');
+        }else if (computer == 'scissors') {
+        console.log('User wins.');
+        }else {
+        console.log('Tie!');
+        }
+    }
+    
+    if (user == 'paper') {
+        if (computer == 'rock') {
+        console.log('User wins.');
+        }else if (computer == 'scissors') {
+        console.log('Computer wins.');
+        }else {
+        console.log('Tie!');
+        }
+    }
+    
+    if (user == 'scissors') {
+        if (computer == 'rock') {
+        console.log('Computer wins.');
+        }else if (computer == 'paper') {
+        console.log('User wins.');
+        }else {
+        console.log('Tie!');
+        }
+    }
+}
+
+const userChoice = "ROCK";
+const computerChoice = getComputerChoice();
+console.log(playRound(userChoice, computerChoice));
+
+const userChoice2 = "Paper";
+const computerChoice2 = getComputerChoice();
+console.log(playRound(userChoice2, computerChoice2));
