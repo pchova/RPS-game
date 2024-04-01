28afd8f4bf69414f6ffbd1892e7883a2c31092c6
Polina Chetnikova <38739507+pchova@users.noreply.github.com>
2024-04-01 15:44:13 -0700

    added a count to keep tracking of which user is winning. Displays the results after a game of five rounds

--- rps.js
+++ rps.js
@@ -1,3 +1,7 @@
+let userCount = 0;
+let compCount = 0;
+let tieCount = 0;
+
 function getComputerChoice() {
     let options = ['rock', 'paper', 'scissors'];
     let choice = Math.floor(Math.random() * options.length);
@@ -12,35 +16,44 @@ function getUserChoice() {
 }
     
 function playRound(userChoice, computerChoice) {
-    let user = userChoice
-    let computer = computerChoice
+    let user = userChoice;
+    let computer = computerChoice;
 
     if (user == 'rock') {
         if (computer == 'paper') {
+            compCount++;
             return 'Computer wins.';
         }else if (computer == 'scissors') {
-        console.log('User wins.');
+            userCount++;
+            return 'User wins.';
         }else {
+            tieCount++;
             return 'Tie!';
         }
     }
     
     if (user == 'paper') {
         if (computer == 'rock') {
+            userCount++;
             return 'User wins.';
         }else if (computer == 'scissors') {
+            compCount++;
             return 'Computer wins.';
         }else {
+            tieCount++;
             return 'Tie!';
         }
     }
     
     if (user == 'scissors') {
         if (computer == 'rock') {
+            compCount++;
             return 'Computer wins.';
         }else if (computer == 'paper') {
+            userCount++;
             return 'User wins.';
         }else {
+            tieCount++;
             return 'Tie!';
         }
     }
@@ -56,6 +69,19 @@ function playGame() {
         console.log("Computer Choice: " + computerChoice);
         console.log(playRound(userChoice, computerChoice));
     }
+    console.log("\n");
+    console.log("---------------Results---------------");
+    console.log("User Count: " + userCount + ", Computer Count: " + compCount + ", Tie: " + tieCount);
+
+    if (userCount > compCount) {
+        console.log("The user wins!!");
+    } else if ( userCount < compCount) {
+        console.log("The computer wins!!");
+    }else if (userCount == compCount) {
+        console.log("It\s a tie!");
+    }else {
+        console.log("Nobody wins.");
+    }
 }
 
-console.log(playGame());
+playGame();
