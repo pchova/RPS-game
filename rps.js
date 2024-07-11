f15954134e8369c6ffa6401cb7fdf83f277ca599
Polina Chetnikova <38739507+pchova@users.noreply.github.com>
2024-07-11 14:54:21 -0700

    added buttons, changed playRound() to listen for events based on user clicks. Plays one round of RPS after the user clicks on their choice

--- rps.js
+++ rps.js
@@ -1,64 +1,107 @@
+/* VARIABLES */
 let userCount = 0;
 let compCount = 0;
 let tieCount = 0;
 
+/*USER BUTTONS ON PAGE */
+const title = document.createElement('h2');
+title.textContent = "To play, click rock, paper, or scissors!";
+const rock = document.createElement('button');
+rock.textContent = "rock";
+const paper = document.createElement('button');
+paper.textContent = "paper";
+const scissors = document.createElement('button');
+scissors.textContent = "scissors";
+
+const buttonContainer = document.querySelector('#choices');
+buttonContainer.appendChild(title);
+buttonContainer.appendChild(rock);
+buttonContainer.appendChild(paper);
+buttonContainer.appendChild(scissors);
+
+rock.addEventListener("click", playRound);
+paper.addEventListener("click", playRound);
+scissors.addEventListener("click", playRound);
+
+
+/*METHODS TO PLAY */
 function getComputerChoice() {
     let options = ['rock', 'paper', 'scissors'];
     let choice = Math.floor(Math.random() * options.length);
 
     return options[choice];
 }
-//console.log(getComputerChoice());
 
 function getUserChoice() {
-    let choice = prompt("To play, type rock, paper, or scissors: ");
+    /* ---- getUserChoice() function before creating the UI with JS -----
+
+    let choice = alert("To play, click rock, paper, or scissors.");
     return choice.toLowerCase();
+    */
 }
-    
-function playRound(userChoice, computerChoice) {
-    let user = userChoice;
-    let computer = computerChoice;
+
+function playRound(e) {
+    let user = e.target.textContent;
+
+    let computer = getComputerChoice();
 
     if (user == 'rock') {
         if (computer == 'paper') {
             compCount++;
-            return 'Computer wins.';
+            console.log('Computer wins.');
         }else if (computer == 'scissors') {
             userCount++;
-            return 'User wins.';
+            console.log('User wins.');
         }else {
             tieCount++;
-            return 'Tie!';
+            console.log('Tie!');
         }
     }
     
     if (user == 'paper') {
         if (computer == 'rock') {
             userCount++;
-            return 'User wins.';
+            console.log('User wins.');
         }else if (computer == 'scissors') {
             compCount++;
-            return 'Computer wins.';
+            console.log('Computer wins.');
         }else {
             tieCount++;
-            return 'Tie!';
+            console.log('Tie!');
         }
     }
     
     if (user == 'scissors') {
         if (computer == 'rock') {
             compCount++;
-            return 'Computer wins.';
+            console.log('Computer wins.');
         }else if (computer == 'paper') {
             userCount++;
-            return 'User wins.';
+            console.log('User wins.');
         }else {
             tieCount++;
-            return 'Tie!';
+            console.log('Tie!');
         }
     }
+
+    console.log("\n");
+    console.log("---------------Results---------------");
+    console.log("User Count: " + userCount + ", Computer Count: " + compCount + ", Tie: " + tieCount);
+    console.log("\n");
 }
 
+
+
+
+
+
+
+
+
+
+
+
+/* 
 function playGame() {
     for(let i = 0; i < 5; i++) {
         const userChoice = getUserChoice();
@@ -69,6 +112,8 @@ function playGame() {
         console.log("Computer Choice: " + computerChoice);
         console.log(playRound(userChoice, computerChoice));
     }
+
+
     console.log("\n");
     console.log("---------------Results---------------");
     console.log("User Count: " + userCount + ", Computer Count: " + compCount + ", Tie: " + tieCount);
@@ -84,4 +129,6 @@ function playGame() {
     }
 }
 
-playGame();
+playGame(); 
+ */
+
