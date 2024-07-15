41d05b4ea3c98ee5f61cf34e0f825ac20128028d
Polina Chetnikova <38739507+pchova@users.noreply.github.com>
2024-07-15 16:19:10 -0700

    RPS game complete! plays 5 rounds and displays winner at the end. Option to click new game to play again

--- rps.js
+++ rps.js
@@ -30,15 +30,23 @@ buttonContainer.appendChild(rock);
 buttonContainer.appendChild(paper);
 buttonContainer.appendChild(scissors);
 
+newGamebtn = document.querySelector("#new-game");
+newGamebtn.textContent = "NEW GAME";
+newGamebtn.setAttribute("style", "padding: 10px; margin: 10px; background-color: pink; border: 3px solid hotpink; border-radius: 10px");
+
 const resultsContainer = document.querySelector('#results');
+const playRoundResults = document.createElement('div');
 const endGameResults = document.createElement('div');
 
 
 /* RPS BUTTON EVENT LISTENERS */
+newGamebtn.addEventListener("click", newGame);
+
 rock.addEventListener("click", () => {
     if(userCount < 5 && compCount < 5) {
         playRound(userChoice = "rock", computerChoice);
-    } else {
+    }
+    if (userCount == 5 || compCount == 5) {
         if (userCount > compCount) {
             endGameResults.textContent = "The user wins!!";
         } else if ( userCount < compCount) {
@@ -55,7 +63,8 @@ rock.addEventListener("click", () => {
 paper.addEventListener("click", () => {
     if(userCount < 5 && compCount < 5) {
         playRound(userChoice = "paper", computerChoice);
-    } else {
+    }
+    if (userCount == 5 || compCount == 5) {
         if (userCount > compCount) {
             endGameResults.textContent = "The user wins!!";
         } else if ( userCount < compCount) {
@@ -72,7 +81,8 @@ paper.addEventListener("click", () => {
 scissors.addEventListener("click", () => {
     if(userCount < 5 && compCount < 5) {
         playRound(userChoice = "scissors", computerChoice);
-    } else {
+    }
+    if (userCount == 5 || compCount == 5) {
         if (userCount > compCount) {
             endGameResults.textContent = "The user wins!!";
         } else if ( userCount < compCount) {
@@ -100,8 +110,6 @@ function playRound(userChoice, computerChoice) {
     let user = userChoice;
     let computer = computerChoice;
     let i = 0;
-
-    const playRoundResults = document.createElement('div');
 
     if (user == 'rock') {
         if (computer == 'paper') {
@@ -165,8 +173,14 @@ function playRound(userChoice, computerChoice) {
 
 
 //new direction of how to program this:
-//delete playGame() and create newGame() function
-//keep score in event listener and end once someone hits 5 points
-//new game restarts score to 0 and there is a button for new game
 
-function newGame() {}
+function newGame() {
+    userCount = 0;
+    compCount = 0;
+    tieCount = 0;
+
+    resultsContainer.removeChild(playRoundResults);
+    resultsContainer.removeChild(endGameResults);
+    resultsContainer.textContent = "Click an option to start new game.";
+    endGameResults.textContent = "";
+}
